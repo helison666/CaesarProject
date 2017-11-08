0ae1a8be3cc14c592a0dfbafb69279ccb7bcbbc0
Ogofmtc <o.v.gofman@gmail.com>
2017-11-08 17:37:19 +0200

    GroupsTable

---
+++ src/main/java/Pages/AdminPageGroupsTable.java
@@ -0,0 +1,21 @@
+package Pages;
+
+import org.openqa.selenium.WebElement;
+
+public class AdminPageGroupsTable {
+
+    WebElement name;
+    WebElement location;
+    WebElement budgetOwner;
+    WebElement direction;
+    WebElement startDate;
+    WebElement finishDate;
+    WebElement teachers;
+    WebElement experts;
+    WebElement stage;
+    WebElement actions;
+    WebElement edit;
+    WebElement delete;
+
+
+}
