c996c30541153cb4417fef360982ef694ed61c1c
vhavritc <havrilyukvv@gmail.com>
2017-11-08 17:35:31 +0200

    Student page

---
+++ src/main/java/Pages/StudentsPage.java
@@ -0,0 +1,14 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class StudentsPage extends MainAdminPage {
+
+    private WebElement headingText;
+
+
+    public StudentsPage(WebDriver driver) {
+        super(driver);
+    }
+}
