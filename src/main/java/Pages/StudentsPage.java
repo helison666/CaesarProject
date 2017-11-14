0b629314908905aca00f8d8b61a48b390243352a
Vitalchi4ik <32616865+Vitalchi4ik@users.noreply.github.com>
2017-11-14 14:19:59 +0200

    Student page

--- src/main/java/Pages/StudentsPage.java
+++ src/main/java/Pages/StudentsPage.java
@@ -1,14 +1,126 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class StudentsPage extends MainAdminPage {
 
+    //Элементы
     private WebElement headingText;
+    private WebElement headerGroupId;
+    private WebElement headerName;
+    private WebElement headerLastName;
+    private WebElement headerEnglishLevel;
+    private WebElement headerCvUrl;
+    private WebElement headerImageUrl;
+    private WebElement headerEntryScore;
+    private WebElement headerApprovedBy;
+    private WebElement headerActions;
 
-
+    //Конструктор
     public StudentsPage(WebDriver driver) {
         super(driver);
+        getStudentsTab().click();
+        this.headingText = driver.findElement(By.tagName("h2"));
+        this.headerGroupId = driver.findElement((By.xpath("//th[text()='groupId']")));
+        this.headerName = driver.findElement((By.xpath("//*[@id=\"students\"]/div/table/thead/tr/th[2]")));
+        this.headerLastName = driver.findElement((By.xpath("//*[@id=\"students\"]/div/table/thead/tr/th[3]")));
+        this.headerEnglishLevel = driver.findElement((By.xpath("//*[@id=\"students\"]/div/table/thead/tr/th[4]")));
+        this.headerCvUrl = driver.findElement((By.xpath("//*[@id=\"students\"]/div/table/thead/tr/th[5]")));
+        this.headerImageUrl = driver.findElement((By.xpath("//*[@id=\"students\"]/div/table/thead/tr/th[6]")));
+        this.headerEntryScore = driver.findElement((By.xpath("//*[@id=\"students\"]/div/table/thead/tr/th[7]")));
+        this.headerApprovedBy = driver.findElement((By.xpath("//*[@id=\"students\"]/div/table/thead/tr/th[8]")));
+        this.headerActions = driver.findElement((By.xpath("//*[@id=\"students\"]/div/table/thead/tr/th[9]")));
     }
+
+    //Геттеры элементов
+    public WebElement getHeading()
+    {
+        return headingText;
+    }
+    public WebElement getHeaderGroupId()
+    {
+        return headerGroupId;
+    }
+    public WebElement getHeaderName()
+    {
+        return headerName;
+    }
+    public WebElement getHeaderLastName()
+    {
+        return  headerLastName;
+    }
+    public WebElement getHeaderEnglishLevel()
+    {
+        return headerEnglishLevel;
+    }
+    public WebElement getHeaderCvUrl()
+    {
+        return headerCvUrl;
+    }
+    public WebElement getHeaderImageUrl()
+    {
+        return headerImageUrl;
+    }
+    public WebElement getHeaderEntryScore()
+    {
+        return headerEntryScore;
+    }
+    public WebElement getHeaderApprovedBy()
+    {
+        return headerApprovedBy;
+    }
+    public WebElement getHeaderActions()
+    {
+        return headerActions;
+    }
+
+
+
+    //Геттеры текста элементов
+    public String getHeadingText()
+    {
+        return getHeading().getText();
+    }
+
+    public String getHeaderGroupIdText()
+    {
+        return getHeaderGroupId().getText();
+    }
+    public String getHeaderNameText()
+    {
+        return getHeaderName().getText();
+    }
+    public String getHeaderLastNameText()
+    {
+        return getHeaderLastName().getText();
+    }
+    public String getHeaderEnglishLevelText()
+    {
+        return getHeaderEnglishLevel().getText();
+    }
+    public String getHeaderCvUrlText()
+    {
+        return getHeaderCvUrl().getText();
+    }
+    public String getHeaderImageUrlText()
+    {
+        return getHeaderImageUrl().getText();
+    }
+    public String getHeaderEntryScoreText()
+    {
+        return getHeaderEntryScore().getText();
+    }
+    public String getHeaderApprovedByText()
+    {
+        return getHeaderApprovedBy().getText();
+    }
+    public String getHeaderActionsText()
+    {
+        return getHeaderActions().getText();
+    }
+
+
+
 }
---
+++ src/main/java/Tests/StudentsPageTest.java
@@ -0,0 +1,62 @@
+package Tests;
+
+import Pages.MainAdminPage;
+import Pages.StudentsPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class StudentsPageTest {
+
+    StudentsPage studPage;
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://localhost:3000/admin");
+        studPage = new StudentsPage(driver);
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    @Test
+    public void getHeadText()
+    {
+        String expected = "Caesar Admin Panel";
+        String actual;
+        actual = studPage.getHeadingText();
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void getHeaderGroupIdText()
+    {
+        String expected = "groupId";
+        String actual;
+        actual = studPage.getHeaderGroupIdText();
+        Assert.assertEquals(actual, expected);
+        //System.out.println(driver.findElement((By.xpath("//table/thead/tr/th[text()='groupId']"))).getText());
+    }
+
+    @Test
+    public void getHeaderNameText()
+    {
+        String expected = "name";
+        String actual;
+        actual = studPage.getHeaderNameText();
+        Assert.assertEquals(actual, expected);
+    }
+
+}
